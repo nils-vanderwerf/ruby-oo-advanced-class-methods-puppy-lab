b6427abf2036219c036321e511f42a47bfe5cd37
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-05-09 10:57:40 +1000

    All dog methods created

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,34 @@
 # Add your code here
+class Dog
+    attr_accessor :name
+    
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def self.all 
+        @@all
+    end
+    
+    def self.print_all
+        puts @@all.map{ |dog| dog.name } #maps through all and accesses dog.name
+    end
+
+    def self.clear_all
+        puts @@all.methods 
+        @@all.clear
+    end
+
+    def save
+        @@all << self
+    end
+end
+
+max = Dog.new("Max")
+milo = Dog.new("Milo")
+sidney = Dog.new("Sidney")
+jackie = Dog.new("Jackie")
+puts Dog.print_all, Dog.all
