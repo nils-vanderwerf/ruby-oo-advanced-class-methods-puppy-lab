b2f928716d66c9a7915a46d672a67677e1343988
Nils van der Werf <n.vanderw.92@gmail.com>
2022-03-14 14:26:14 +1100

    created puppy methods

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,34 +1,30 @@
-# Add your code here
 class Dog
-    attr_accessor :name
-    
     @@all = []
 
     def initialize(name)
         @name = name
-        save
+        self.save
     end
 
-    def self.all 
-        @@all
+    def name
+        @name
     end
-    
-    def self.print_all
-        puts @@all.map{ |dog| dog.name } #maps through all and accesses dog.name
+
+    def self.all
+        @@all
     end
 
     def self.clear_all
-        puts @@all.methods 
-        @@all.clear
+        @@all = []
+    end
+
+    def self.print_all
+        @@all.each do |dog|
+            puts dog.name
+        end
     end
 
     def save
         @@all << self
     end
 end
-
-max = Dog.new("Max")
-milo = Dog.new("Milo")
-sidney = Dog.new("Sidney")
-jackie = Dog.new("Jackie")
-puts Dog.print_all, Dog.all
